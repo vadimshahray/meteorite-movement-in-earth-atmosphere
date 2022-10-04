be6392700b7bfcb40a2389ea4631fe44ec627c9e
vadimshahray <vadimshahray.workmail@gmail.com>
2022-10-04 22:58:25 +0700

    style: remove padding top

--- src/layouts/ConfigureLayout/ConfigureLayout.tsx
+++ src/layouts/ConfigureLayout/ConfigureLayout.tsx
@@ -1,10 +1,16 @@
 import { Container } from 'components'
 import React from 'react'
+import { styleClasses } from 'utils'
+import styles from './configureLayout.module.css'
 
 /**
  * Блок, содержащий все, что связано с настройкой физических моделей (поля ввода данных и т.п.)
  * @returns {JSX.Element}
  */
-export const ConfigureLayout = (props: LayoutProps) => {
-  return <Container {...props}>Config</Container>
+export const ConfigureLayout = ({ className, ...props }: LayoutProps) => {
+  return (
+    <Container className={styleClasses(styles.layout, className)} {...props}>
+      Config
+    </Container>
+  )
 }
