0d8c3c45e1034a1a3ffaa134e74f6a303697e146
vadimshahray <vadimshahray.workmail@gmail.com>
2022-10-05 13:13:39 +0700

    style: change root containers and configure width

--- src/layouts/ConfigureLayout/ConfigureToolbar/ConfigureToolbar.tsx
+++ src/layouts/ConfigureLayout/ConfigureToolbar/ConfigureToolbar.tsx
@@ -1,12 +1,13 @@
 import { Container, ThemeButtonContainer } from 'components'
 import React from 'react'
+import styles from './configureToolbar.module.css'
 
 /**
  * Тулбар секции ввода данных ConfigureLayout
  */
 export const ConfigureToolbar = () => {
   return (
-    <Container>
+    <Container className={styles.layout}>
       {'Ввод данных'}
       <ThemeButtonContainer />
     </Container>
