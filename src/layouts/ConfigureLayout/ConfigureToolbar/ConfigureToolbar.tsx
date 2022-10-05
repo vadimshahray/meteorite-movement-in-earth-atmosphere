129257d6871fdf339b6b2f7e5a20d7b3324cd40f
vadimshahray <vadimshahray.workmail@gmail.com>
2022-10-05 12:38:43 +0700

    feat: add configure toolbar

---
+++ src/layouts/ConfigureLayout/ConfigureToolbar/ConfigureToolbar.tsx
@@ -0,0 +1,14 @@
+import { Container, ThemeButtonContainer } from 'components'
+import React from 'react'
+
+/**
+ * Тулбар секции ввода данных ConfigureLayout
+ */
+export const ConfigureToolbar = () => {
+  return (
+    <Container>
+      {'Ввод данных'}
+      <ThemeButtonContainer />
+    </Container>
+  )
+}
