2fac227ea33483cfb22e19bfc4abd0fd91ca3a80
vadimshahray <vadimshahray.workmail@gmail.com>
2022-10-06 22:08:46 +0700

    refact: replace components props, add index files

--- src/components/presentational/ThemeButton/ThemeButton.tsx
+++ src/components/presentational/ThemeButton/ThemeButton.tsx
@@ -2,6 +2,17 @@ import DarkModeIcon from '@mui/icons-material/DarkMode'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import { IconButton } from '@mui/material'
 
+/** Пропсы компонента ThemeButton */
+export type ThemeButtonProps = {
+  /** Начальное значение модификатора цвета темы */
+  colorMode: ColorMode
+  /**
+   * Обработчик изменения модификатора цвета темы
+   * @param {ColorMode} newColorMode Новое значение модификатора цвета темы
+   */
+  onChange(newColorMode: ColorMode): void
+}
+
 /**
  * Кнопка-переключатель темы приложения (light/dark)
  * @returns {JSX.Element}
