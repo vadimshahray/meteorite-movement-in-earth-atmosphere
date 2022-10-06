7e4a807ad124b7560b792034815584b3906a903b
vadimshahray <vadimshahray.workmail@gmail.com>
2022-10-06 22:01:30 +0700

    style: add modelingLayout background

--- src/layouts/ModelingLayout/ModelingLayout.tsx
+++ src/layouts/ModelingLayout/ModelingLayout.tsx
@@ -1,10 +1,19 @@
 import { FullContainer } from 'components'
 import React from 'react'
+import { styleClasses } from 'utils'
+import styles from './modelingLayout.module.css'
 
 /**
  * Блок, содержащий 3D модель
  * @returns {JSX.Element}
  */
 export const ModelingLayout = (props: LayoutProps) => {
-  return <FullContainer {...props}>3d modeling layout</FullContainer>
+  return (
+    <FullContainer
+      {...props}
+      className={styleClasses(props.className, styles.layout)}
+    >
+      3d modeling layout
+    </FullContainer>
+  )
 }
