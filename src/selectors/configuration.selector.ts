aac0bab295ecf784cc891117469cc18201ef1d0a
shaxraj.2021 <shaxraj.2021@stud.nstu.ru>
2022-10-08 11:23:07 +0700

    feat: add planet templates slice

--- src/selectors/configuration.selector.ts
+++ src/selectors/configuration.selector.ts
@@ -5,3 +5,11 @@
  */
 export const selectActiveSection = (state: RootState) =>
   state.configuration.activeSection
+
+/**
+ * Селектор данных активного шаблона планет
+ * @param {RootState} state Внутреннее состояние слайса
+ * @returns {PlanetData} Данные планеты
+ */
+export const selectActivePlanetTemplateData = (state: RootState) =>
+  state.configuration.planetTemplates[state.configuration.activePlanetTemplate]
