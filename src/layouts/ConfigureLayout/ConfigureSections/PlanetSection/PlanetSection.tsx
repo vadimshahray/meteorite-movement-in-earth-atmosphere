6da1808d2fd27ce683d9867de0d2bb28434e1567
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-10-24 11:18:19 +0700

    feat: add g variable view to planet section

---
+++ src/layouts/ConfigureLayout/ConfigureSections/PlanetSection/PlanetSection.tsx
@@ -0,0 +1,10 @@
+import { GVariableView } from './Fields/GVariableView'
+
+/** Секция конфигурации "Планета" */
+export const PlanetSection = () => {
+  return (
+    <div>
+      <GVariableView />
+    </div>
+  )
+}
