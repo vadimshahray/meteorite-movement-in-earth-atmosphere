0e1bf927660d0c041c0db113c818a71c414b3637
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-10-24 10:43:58 +0700

    style: disable breakpoints

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,15 @@ import { createTheme } from '@mui/material'
 
 /** Объект темы приложения */
 export const theme = createTheme({
+  breakpoints: {
+    values: {
+      sm: 7000,
+      md: 7000,
+      lg: 7000,
+      xs: 7000,
+      xl: 7000,
+    },
+  },
   shape: {
     borderRadius: 8,
   },
