ebca67c7df4291bf8141eaccde1b7c931388a835
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-10-25 20:32:50 +0700

    feat: add planet common data selector

--- src/selectors/configuration.selector.ts
+++ src/selectors/configuration.selector.ts
@@ -15,6 +15,20 @@ export const selectPlanetModels = (state: RootState) =>
   state.configuration.planetModels
 
 /**
+ * Селектор массы планеты
+ * @param {RooState} state Внутреннее состояние хранилища
+ * @returns {number} Масса планеты
+ */
+export const selectPlanetM = (state: RootState) => state.configuration.planet.M
+
+/**
+ * Селектор радиуса планеты
+ * @param {RooState} state Внутреннее состояние хранилища
+ * @returns {number} Радиус планеты
+ */
+export const selectPlanetR = (state: RootState) => state.configuration.planet.R
+
+/**
  * Селектор активного ввода данных g
  * @param {RooState} state Внутреннее состояние хранилища
  * @returns {'short' | 'detailed'} Активный вариант ввода данных
