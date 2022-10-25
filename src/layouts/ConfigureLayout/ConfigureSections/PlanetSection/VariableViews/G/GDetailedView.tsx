63333c08e96c2fc66d76740f779659ff803a739f
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-10-25 20:34:28 +0700

    feat: select planet common data to GDetailedView

--- src/layouts/ConfigureLayout/ConfigureSections/PlanetSection/VariableViews/G/GDetailedView.tsx
+++ src/layouts/ConfigureLayout/ConfigureSections/PlanetSection/VariableViews/G/GDetailedView.tsx
@@ -1,16 +1,18 @@
 import { TextField } from '@mui/material'
 import { Container } from 'components'
 import { useSelector } from 'react-redux'
-import { selectPlanetgG } from 'selectors'
+import { selectPlanetgG, selectPlanetM, selectPlanetR } from 'selectors'
 
 export const GDetailedView = () => {
   const G = useSelector(selectPlanetgG)
+  const M = useSelector(selectPlanetM)
+  const R = useSelector(selectPlanetR)
 
   return (
     <Container disableGuttersH>
       <TextField label='Гравитационная постоянная' value={G.view} disabled />
-      <TextField label='Радиус планеты' />
-      <TextField label='Масса планеты' />
+      <TextField label='Радиус планеты' value={M} />
+      <TextField label='Масса планеты' value={R} />
     </Container>
   )
 }
