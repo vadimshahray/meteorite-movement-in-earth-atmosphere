0a3ff5b6c4161b83e39ca9a4bfb9a22f22968b10
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-10-25 19:10:31 +0700

    refactor:  change planet data model

--- src/models/planets/default.ts
+++ src/models/planets/default.ts
@@ -2,13 +2,13 @@ import { G } from './constants'
 
 /** Дефолтная конфигурация планеты */
 export const defaultPlanet: PlanetData = {
+  M: 0,
+  R: 0,
   g: {
     active: 'short',
     short: 0,
     detailed: {
       G,
-      M: 0,
-      R: 0,
     },
   },
 }
--- src/models/planets/earth.ts
+++ src/models/planets/earth.ts
@@ -2,13 +2,13 @@ import { G } from './constants'
 
 /** Дефолтная конфигурация планеты "Земля" */
 export const EarthPlanet: PlanetData = {
+  M: 0, // TODO: fill
+  R: 0, // TODO: fill
   g: {
     active: 'short',
     short: 9.87, //TODO: уточнить
     detailed: {
       G,
-      M: 0, // TODO: fill
-      R: 0, // TODO: fill
     },
   },
 }
