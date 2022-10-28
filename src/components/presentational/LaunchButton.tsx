03dd1f76b72896e138864a9987c79bce75a2b418
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-10-29 00:04:27 +0700

    feat: add LaunchButton onClick handler

--- src/components/presentational/LaunchButton.tsx
+++ src/components/presentational/LaunchButton.tsx
@@ -1,11 +1,15 @@
 import PlayArrowIcon from '@mui/icons-material/PlayArrow'
 import { Button } from 'components'
 
+export type LaunchButtonProps = {
+  onClick: () => unknown
+}
+
 /** Кнопка запуска моделирования */
-export const LaunchButton = () => {
+export const LaunchButton = ({ onClick }: LaunchButtonProps) => {
   return (
-    <Button variant='contained' startIcon={<PlayArrowIcon />}>
-      {'Моделировать'}
+    <Button variant='contained' startIcon={<PlayArrowIcon />} onClick={onClick}>
+      Моделировать
     </Button>
   )
 }
