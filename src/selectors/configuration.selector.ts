4953310f3bce044dd4ccfe40eebd71fe36c16158
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-10-29 10:22:37 +0700

    chore: delete useless configure sections selector

--- src/selectors/configuration.selector.ts
+++ src/selectors/configuration.selector.ts
@@ -1,12 +1,4 @@
 /**
- * Селектор активной секции конфигурации
- * @param {RooState} state Внутреннее состояние хранилища
- * @returns {number} Индекс активной секции
- */
-export const selectActiveSection = (state: RootState) =>
-  state.configuration.activeSection
-
-/**
  * Селектор моделей планет
  * @param {RooState} state Внутреннее состояние хранилища
  * @returns {Record<keyof PlanetModels, string>} Модели планет
