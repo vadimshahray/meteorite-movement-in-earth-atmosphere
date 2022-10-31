563d9a30ea0443fc1346385fa697fb8369405a55
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-10-31 13:06:16 +0700

    feat: add planet M field

--- src/layouts/ConfigureLayout/ConfigureSections/PlanetSection/PlanetSection.tsx
+++ src/layouts/ConfigureLayout/ConfigureSections/PlanetSection/PlanetSection.tsx
@@ -1,11 +1,12 @@
 import { GVariableView } from './VariableViews'
-import { PlanetRField } from '../Fields'
+import { PlanetRField, PlanetMField } from '../Fields'
 
 /** Секция конфигурации "Планета" */
 export const PlanetSection = () => {
   return (
     <div>
       <PlanetRField />
+      <PlanetMField />
       <GVariableView />
     </div>
   )
