ff4e2acf31143feebc805d92ac6e162dcc8d1315
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-10-31 11:34:07 +0700

    feat: add g short data validation

--- src/layouts/ConfigureLayout/ConfigureForm.tsx
+++ src/layouts/ConfigureLayout/ConfigureForm.tsx
@@ -1,15 +1,26 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import { Container, LaunchButton } from 'components'
 import { FormProvider, useForm } from 'react-hook-form'
+import * as yup from 'yup'
 import { ConfigureSections } from './ConfigureSections'
 
 type Fields = {
   g: number
 }
 
+const schema = yup.object({
+  g: yup
+    .number()
+    .typeError('Не число')
+    .positive('Не положительное число')
+    .required('Обязательно'),
+})
+
 /** Содержит поля ввода для настроек моделей */
 export const ConfigureForm = () => {
   const form = useForm<Fields>({
     mode: 'onChange',
+    resolver: yupResolver(schema),
   })
 
   const handleLaunch = () => {}
