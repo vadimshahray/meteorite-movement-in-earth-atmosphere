4c4101a9e97e3c9f96f4c5ff6012080be8cecf73
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-05 10:35:00 +0700

    feat: delete `react-hook-form`

--- src/layouts/ConfigureLayout/ConfigureForm.tsx
+++ src/layouts/ConfigureLayout/ConfigureForm.tsx
@@ -1,67 +1,13 @@
-import { yupResolver } from '@hookform/resolvers/yup'
 import { Container, LaunchButton } from 'components'
-import { useDispatch } from 'hooks'
-import { FormProvider, useForm } from 'react-hook-form'
-import { setPlanetData } from 'slices'
-import { NumberCommaToDot } from 'utils'
-import * as yup from 'yup'
 import { ConfigureSections } from './ConfigureSections'
-
-type Fields = {
-  R: number
-  M: number
-  g: number
-}
-
-const schema = yup.object({
-  R: yup
-    .number()
-    .transform(NumberCommaToDot)
-    .typeError('Не число')
-    .positive('Не положительное число')
-    .required('Обязательно'),
-  M: yup
-    .number()
-    .transform(NumberCommaToDot)
-    .typeError('Не число')
-    .positive('Не положительное число')
-    .required('Обязательно'),
-  g: yup
-    .number()
-    .transform(NumberCommaToDot)
-    .typeError('Не число')
-    .positive('Не положительное число')
-    .required('Обязательно'),
-})
 
 /** Содержит поля ввода для настроек моделей */
 export const ConfigureForm = () => {
-  const dispatch = useDispatch()
-
-  const form = useForm<Fields>({
-    mode: 'onChange',
-    resolver: yupResolver(schema),
-  })
-
-  const handleLaunch = () => {
-    form.handleSubmit(({ M, R, g }) => {
-      dispatch(
-        setPlanetData({
-          M,
-          R,
-          g: {
-            short: g,
-          },
-        }),
-      )
-    })()
-  }
+  const handleLaunch = () => {}
 
   return (
     <Container>
-      <FormProvider {...form}>
-        <ConfigureSections />
-      </FormProvider>
+      <ConfigureSections />
 
       <LaunchButton onClick={handleLaunch} />
     </Container>
