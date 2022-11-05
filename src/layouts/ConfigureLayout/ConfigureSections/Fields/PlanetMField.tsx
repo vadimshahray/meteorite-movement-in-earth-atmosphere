c5da34418d967f5c46a3746b2711e66a6bdbd566
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-05 11:16:19 +0700

    feat: use isolated validation in fields

--- src/layouts/ConfigureLayout/ConfigureSections/Fields/PlanetMField.tsx
+++ src/layouts/ConfigureLayout/ConfigureSections/Fields/PlanetMField.tsx
@@ -1,9 +1,39 @@
-import { TextField } from '@mui/material'
+import { ValidatedTextField } from 'components'
+import { useDispatch } from 'hooks'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import { selectPlanetM } from 'selectors'
+import { setPlanetData } from 'slices'
+import { positiveNumberRule } from 'utils'
+import { object } from 'yup'
+
+const schema = object({
+  M: positiveNumberRule,
+})
+
+const extractValueObject = (input: string) => ({
+  M: input,
+})
 
 export const PlanetMField = () => {
+  const dispatch = useDispatch()
   const M = useSelector(selectPlanetM)
 
-  return <TextField label='Масса планеты' />
+  const handleValid = (data: any) => {
+    dispatch(
+      setPlanetData({
+        M: data.M as number,
+      }),
+    )
+  }
+
+  return (
+    <ValidatedTextField
+      label='Масса планеты'
+      value={M.toString()}
+      schema={schema}
+      extractValueObject={extractValueObject}
+      onValid={handleValid}
+    />
+  )
 }
