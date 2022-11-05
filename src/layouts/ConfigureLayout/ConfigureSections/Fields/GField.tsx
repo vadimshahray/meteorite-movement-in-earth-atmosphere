a0c20df24ad4dcaf4a21cdc19108bdb3812ce8d3
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-05 17:54:01 +0700

    feat: use `NumberFieldGroup` & `PowerTextField`

--- src/layouts/ConfigureLayout/ConfigureSections/Fields/GField.tsx
+++ src/layouts/ConfigureLayout/ConfigureSections/Fields/GField.tsx
@@ -1,41 +1,60 @@
-import { ValidatedTextField } from 'components'
+import { NumberFieldGroup, ValidatedTextField } from 'components'
 import { useDispatch } from 'hooks'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useSelector } from 'react-redux'
 import { selectPlanetg } from 'selectors'
 import { setPlanetData } from 'slices'
 import { positiveNumberRule } from 'utils'
 import { object } from 'yup'
+import { PowerTextField } from './PowerTextField'
 
 const schema = object({
-  g: positiveNumberRule,
+  value: positiveNumberRule,
 })
 
 const extractValueObject = (input: string) => ({
-  g: input,
+  value: input,
 })
 
 export const GField = () => {
   const dispatch = useDispatch()
   const g = useSelector(selectPlanetg)
 
-  const handleValid = (data: any) => {
-    dispatch(
-      setPlanetData({
-        g: {
-          short: data.g as number,
-        },
-      }),
-    )
-  }
+  const handleValid = useCallback(
+    ({ value, power }: Partial_PowerNumber) => {
+      dispatch(
+        setPlanetData({
+          g: { short: { value, power } },
+        }),
+      )
+    },
+    [dispatch],
+  )
+
+  const handleValueValid = useCallback(
+    (data: any) => {
+      handleValid({ value: data.value as number })
+    },
+    [handleValid],
+  )
+  const handlePowerValid = useCallback(
+    (power: number) => {
+      handleValid({ power })
+    },
+    [handleValid],
+  )
 
   return (
-    <ValidatedTextField
-      value={g.toString()}
-      label='Ускорение свободного падения'
-      schema={schema}
-      onValid={handleValid}
-      extractValueObject={extractValueObject}
-    />
+    <NumberFieldGroup>
+      <ValidatedTextField
+        value={g.value.toString()}
+        label='Ускорение свободного падения'
+        schema={schema}
+        onValid={handleValueValid}
+        extractValueObject={extractValueObject}
+      />
+
+      <PowerTextField power={g.power} onValid={handlePowerValid} />
+    </NumberFieldGroup>
   )
 }
--- src/layouts/ConfigureLayout/ConfigureSections/Fields/PlanetMField.tsx
+++ src/layouts/ConfigureLayout/ConfigureSections/Fields/PlanetMField.tsx
@@ -1,39 +1,60 @@
-import { ValidatedTextField } from 'components'
+import { NumberFieldGroup, ValidatedTextField } from 'components'
 import { useDispatch } from 'hooks'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useSelector } from 'react-redux'
 import { selectPlanetM } from 'selectors'
 import { setPlanetData } from 'slices'
 import { positiveNumberRule } from 'utils'
 import { object } from 'yup'
+import { PowerTextField } from './PowerTextField'
 
 const schema = object({
-  M: positiveNumberRule,
+  value: positiveNumberRule,
 })
 
 const extractValueObject = (input: string) => ({
-  M: input,
+  value: input,
 })
 
 export const PlanetMField = () => {
   const dispatch = useDispatch()
   const M = useSelector(selectPlanetM)
 
-  const handleValid = (data: any) => {
-    dispatch(
-      setPlanetData({
-        M: data.M as number,
-      }),
-    )
-  }
+  const handleValid = useCallback(
+    ({ value, power }: Partial_PowerNumber) => {
+      dispatch(
+        setPlanetData({
+          M: { value, power },
+        }),
+      )
+    },
+    [dispatch],
+  )
+
+  const handleValueValid = useCallback(
+    (data: any) => {
+      handleValid({ value: data.value as number })
+    },
+    [handleValid],
+  )
+  const handlePowerValid = useCallback(
+    (power: number) => {
+      handleValid({ power })
+    },
+    [handleValid],
+  )
 
   return (
-    <ValidatedTextField
-      label='Масса планеты'
-      value={M.toString()}
-      schema={schema}
-      extractValueObject={extractValueObject}
-      onValid={handleValid}
-    />
+    <NumberFieldGroup>
+      <ValidatedTextField
+        label='Масса планеты'
+        value={M.value.toString()}
+        schema={schema}
+        extractValueObject={extractValueObject}
+        onValid={handleValueValid}
+      />
+
+      <PowerTextField power={M.power} onValid={handlePowerValid} />
+    </NumberFieldGroup>
   )
 }
