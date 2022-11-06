0fb99b3a78a032a2fea8664fe966062dfd006a48
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-06 23:57:53 +0700

    feat: add `DialogContext.close()` method

--- src/providers/DialogProvider.tsx
+++ src/providers/DialogProvider.tsx
@@ -5,19 +5,24 @@ type DialogType = ReactElement<DialogProps, typeof Dialog>
 
 export const DialogContext = React.createContext<{
   show: (dialog: DialogType) => void
+  close: () => void
 }>({
   show: (_: DialogType) => {},
+  close: () => {},
 })
 
 export const DialogProvider = ({ children }: PropsWithChildren) => {
   const [dialog, setDialog] = useState<DialogType>()
 
-  const showDialog = (dialog: DialogType) => {
+  const show = (dialog: DialogType) => {
     setDialog(dialog)
+  }
+  const close = () => {
+    setDialog(undefined)
   }
 
   return (
-    <DialogContext.Provider value={{ show: showDialog }}>
+    <DialogContext.Provider value={{ show, close }}>
       <>
         {children}
 
