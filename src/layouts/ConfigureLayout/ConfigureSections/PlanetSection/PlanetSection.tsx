1ea2f954d978fab0a88fab363bc7d3d3e407b4cf
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-07 12:24:46 +0700

    feat: remove action button when section unmount

--- src/layouts/ConfigureLayout/ConfigureSections/PlanetSection/PlanetSection.tsx
+++ src/layouts/ConfigureLayout/ConfigureSections/PlanetSection/PlanetSection.tsx
@@ -1,13 +1,23 @@
+import { useConfigureToolbar } from 'hooks'
+import React, { useEffect } from 'react'
 import { PickPlanetModelDataButton } from './PickPlanetModelDataButton'
 import { GVariableView } from './VariableViews'
 import { PlanetRField, PlanetMField } from '../Fields'
 
 /** Секция конфигурации "Планета" */
 export const PlanetSection = () => {
+  const { setActionButton } = useConfigureToolbar()
+
+  useEffect(() => {
+    setActionButton(<PickPlanetModelDataButton />)
+
+    return () => {
+      setActionButton()
+    }
+  }, [setActionButton])
+
   return (
     <div>
-      <PickPlanetModelDataButton />
-
       <PlanetRField />
       <PlanetMField />
       <GVariableView />
