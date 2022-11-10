fde17dae15db1787a15a0493ffe8efd2bfc8b04c
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-10 19:33:29 +0700

    feat: add `PickPhysicalObjectModelButton` comp

---
+++ src/layouts/ConfigureLayout/ConfigureSections/SubjectSection/PickPhysicalObjectModelButton.tsx
@@ -0,0 +1,12 @@
+import ViewInArIcon from '@mui/icons-material/ViewInAr'
+import { IconButton } from '@mui/material'
+
+export const PickPhysicalObjectModelButton = () => {
+  const handleClick = () => {}
+
+  return (
+    <IconButton onClick={handleClick}>
+      <ViewInArIcon />
+    </IconButton>
+  )
+}
--- src/layouts/ConfigureLayout/ConfigureSections/SubjectSection/SubjectSection.tsx
+++ src/layouts/ConfigureLayout/ConfigureSections/SubjectSection/SubjectSection.tsx
@@ -1,7 +1,18 @@
+import { useConfigureToolbar } from 'hooks'
+import { useEffect } from 'react'
 import { KField, MField, RField, SField, YField } from './Fields'
+import { PickPhysicalObjectModelButton } from './PickPhysicalObjectModelButton'
 
 /** Секция конфигурации "Исследуемый объект" */
 export const SubjectSection = () => {
+  const { setActionButton } = useConfigureToolbar()
+
+  useEffect(() => {
+    setActionButton(<PickPhysicalObjectModelButton />)
+
+    return () => setActionButton()
+  })
+
   return (
     <div>
       <MField />
