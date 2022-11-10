1a197abc4652eb74511306b198ed3f5223164222
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-10 21:20:23 +0700

    feat: add `SectionsTabs` component

--- src/layouts/ConfigureLayout/ConfigureSections/ConfigureSections.tsx
+++ src/layouts/ConfigureLayout/ConfigureSections/ConfigureSections.tsx
@@ -5,35 +5,30 @@ import { useSelector } from 'react-redux'
 import { selectIsUserSectionInputValid } from 'selectors'
 import { AtmosphereSection } from './AtmosphereSection'
 import { PlanetSection } from './PlanetSection'
+import { SectionsTabs } from './SectionsTabs'
 import { SubjectSection } from './SubjectSection'
 
 /** Секции конфигурации */
 export const ConfigureSections = () => {
-  const isUserInputValid = useSelector(selectIsUserSectionInputValid)
+  const [activeTabPanel, setActiveTabPanel] = useState('0')
 
-  const [activeTab, setActiveTab] = useState('0')
-
-  const handleChange = (_: any, newActiveTab: number) => {
-    setActiveTab(newActiveTab.toString())
+  const handleActiveTabChange = (tab: number) => {
+    setActiveTabPanel(tab.toString())
   }
 
   return (
     <Container disableGutters>
       <Container disableGutters>
-        <Tabs value={activeTab} onChange={handleChange} variant='fullWidth'>
-          <Tab label='Объект' value='0' disabled={isUserInputValid} />
-          <Tab label='Планета' value='1' disabled={isUserInputValid} />
-          <Tab label='Атмосфера' value='2' disabled={isUserInputValid} />
-        </Tabs>
+        <SectionsTabs onActiveTabChange={handleActiveTabChange} />
       </Container>
 
-      <TabPanel value={activeTab} index='0'>
+      <TabPanel value={activeTabPanel} index='0'>
         <SubjectSection />
       </TabPanel>
-      <TabPanel value={activeTab} index='1'>
+      <TabPanel value={activeTabPanel} index='1'>
         <PlanetSection />
       </TabPanel>
-      <TabPanel value={activeTab} index='2'>
+      <TabPanel value={activeTabPanel} index='2'>
         <AtmosphereSection />
       </TabPanel>
     </Container>
---
+++ src/layouts/ConfigureLayout/ConfigureSections/SectionsTabs.tsx
@@ -0,0 +1,33 @@
+import { Tab, Tabs } from '@mui/material'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { selectIsUserSectionInputValid } from 'selectors'
+
+export type SectionsTabsProps = {
+  onActiveTabChange: (value: number) => void
+}
+
+export const SectionsTabs = ({
+  onActiveTabChange: onActiveTabChanged,
+}: SectionsTabsProps) => {
+  const isUserInputValid = useSelector(selectIsUserSectionInputValid)
+
+  const [activeTab, setActiveTab] = useState('0')
+
+  const handleChange = (_: any, newActiveTab: number) => {
+    setActiveTab(newActiveTab.toString())
+    onActiveTabChanged(newActiveTab)
+  }
+
+  return (
+    <Tabs
+      value={isUserInputValid ? activeTab : -1}
+      onChange={handleChange}
+      variant='fullWidth'
+    >
+      <Tab label='Объект' value='0' disabled={!isUserInputValid} />
+      <Tab label='Планета' value='1' disabled={!isUserInputValid} />
+      <Tab label='Атмосфера' value='2' disabled={!isUserInputValid} />
+    </Tabs>
+  )
+}
