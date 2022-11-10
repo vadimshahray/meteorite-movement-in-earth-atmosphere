462657e2e9668ed7d57a5bf287f05d371394dd9b
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-10 19:25:59 +0700

    feat: add physical object data fields

---
+++ src/layouts/ConfigureLayout/ConfigureSections/SubjectSection/Fields/SField.tsx
@@ -0,0 +1,28 @@
+import { ValidatedTextField } from 'components'
+import { useDispatch } from 'hooks'
+import { useSelector } from 'react-redux'
+import { selectObjectModelS } from 'selectors'
+import { setPhysicalModelData } from 'slices'
+import { positiveNumberRule } from 'utils'
+
+export const SField = () => {
+  const dispatch = useDispatch()
+  const S = useSelector(selectObjectModelS)
+
+  const handleValid = (value: number) => {
+    dispatch(
+      setPhysicalModelData({
+        S: value,
+      }),
+    )
+  }
+
+  return (
+    <ValidatedTextField
+      label='Площадь поперечного сечения, м²'
+      value={S.toString()}
+      rule={positiveNumberRule}
+      onValid={handleValid}
+    />
+  )
+}
--- src/layouts/ConfigureLayout/ConfigureSections/SubjectSection/SubjectSection.tsx
+++ src/layouts/ConfigureLayout/ConfigureSections/SubjectSection/SubjectSection.tsx
@@ -1,10 +1,13 @@
-import { MField, RField } from './Fields'
+import { KField, MField, RField, SField, YField } from './Fields'
 
 /** Секция конфигурации "Исследуемый объект" */
 export const SubjectSection = () => {
   return (
     <div>
       <MField />
+      <KField />
+      <SField />
+      <YField />
       <RField />
     </div>
   )
