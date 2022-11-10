11211af621eddcc4879054fc4368c9734d3c8635
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-10 19:53:26 +0700

    feat: show `PhysicalModelsListDialog` on click

--- src/layouts/ConfigureLayout/ConfigureSections/SubjectSection/PickPhysicalObjectModelButton.tsx
+++ src/layouts/ConfigureLayout/ConfigureSections/SubjectSection/PickPhysicalObjectModelButton.tsx
@@ -1,8 +1,14 @@
 import ViewInArIcon from '@mui/icons-material/ViewInAr'
 import { IconButton } from '@mui/material'
+import { useDialog } from 'hooks'
+import { PhysicalModelsListDialog } from './PhysicalModelsListDialog'
 
 export const PickPhysicalObjectModelButton = () => {
-  const handleClick = () => {}
+  const { show, close } = useDialog()
+
+  const handleClick = () => {
+    show(<PhysicalModelsListDialog onClose={close} />)
+  }
 
   return (
     <IconButton onClick={handleClick}>
