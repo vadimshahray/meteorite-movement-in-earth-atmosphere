bfcc9f5c92d6b4923aec844a69c361ab5d1a3c5e
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-12 10:48:54 +0700

    refactor: delete planet logic from configuration

--- src/slices/configuration.slice.ts
+++ src/slices/configuration.slice.ts
@@ -1,13 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
-import { defaultPlanet } from 'models'
-import { planetModels, setDefinedProperties } from 'utils'
 
 const initialState: ConfigurationSliceState = {
-  planet: defaultPlanet,
-  planetModels: [
-    { key: '@Earth', name: 'Земля' },
-    { key: '@Mars', name: 'Марс' },
-  ],
   isUserSectionInputValid: true,
 }
 
@@ -18,19 +11,10 @@ export const configurationSlice = createSlice<
   name: 'configuration',
   initialState,
   reducers: {
-    setPlanetModelData: (state, { payload }) => {
-      state.planet = planetModels[payload]
-    },
-
-    setPlanetData: (state, { payload }) => {
-      setDefinedProperties(state.planet, payload)
-    },
-
     setIsUserSectionInputValid: (state, { payload }) => {
       state.isUserSectionInputValid = payload
     },
   },
 })
 
-export const { setPlanetModelData, setPlanetData, setIsUserSectionInputValid } =
-  configurationSlice.actions
+export const { setIsUserSectionInputValid } = configurationSlice.actions
--- src/types/slices/configuration.slice.d.ts
+++ src/types/slices/configuration.slice.d.ts
@@ -1,10 +1,5 @@
 /** Состояние слайса конфигурации приложения */
 type ConfigurationSliceState = {
-  /** Вводимые данные планеты */
-  planet: PlanetData
-  /** Модели планет */
-  planetModels: PlanetModel[]
-
   /** Данные секции, введенные пользователем, валидны */
   isUserSectionInputValid: boolean
 }
@@ -15,26 +10,6 @@ type ConfigurationSliceState = {
  */
 interface ConfigurationSlice
   extends SliceCaseReducers<ConfigurationSliceState> {
-  /**
-   * Устанавливает данные модели планет в качестве вводимых данных планеты
-   * @param {ConfigurationSliceState} state Внутреннее состояние слайса
-   * @param {PayloadAction<keyof PlanetModels>} action Модель планеты
-   */
-  setPlanetModelData(
-    state: ConfigurationSliceState,
-    action: PayloadAction<keyof PlanetModels>,
-  ): void
-
-  /**
-   * Устанавливает данные модели планеты, которые ввел пользователь
-   * @param {ConfigurationSliceState} state Внутреннее состояние слайса
-   * @param {PayloadAction<Partial_PlanetData>} action Данные модели планеты, введенные пользователем
-   */
-  setPlanetData(
-    state: ConfigurationSliceState,
-    action: PayloadAction<Partial_PlanetData>,
-  ): void
-
   setIsUserSectionInputValid(
     state: ConfigurationSliceState,
     action: PayloadAction<boolean>,
