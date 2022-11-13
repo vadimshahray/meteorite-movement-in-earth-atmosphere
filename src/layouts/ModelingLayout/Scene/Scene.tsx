86b87164fa3be328792d62fac80bc29aebea2900
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-13 10:52:51 +0700

    feat: add stars

--- src/layouts/ModelingLayout/Scene/Scene.tsx
+++ src/layouts/ModelingLayout/Scene/Scene.tsx
@@ -1,3 +1,4 @@
+import { Stars } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 
@@ -6,6 +7,15 @@ export const Scene = () => {
     <Canvas>
       <ambientLight />
 
+      <Stars
+        radius={100}
+        depth={50}
+        count={5000}
+        factor={4}
+        saturation={0}
+        fade
+        speed={1}
+      />
       <mesh>
         <boxGeometry />
         <meshBasicMaterial color='red' />
