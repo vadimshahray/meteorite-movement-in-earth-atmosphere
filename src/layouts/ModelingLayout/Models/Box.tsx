404372f7c1d1817d372b9871fdd3f3145b43f165
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-20 18:17:00 +0700

    feat: memoize models

--- src/layouts/ModelingLayout/Models/Box.tsx
+++ src/layouts/ModelingLayout/Models/Box.tsx
@@ -1,10 +1,14 @@
 import { RoundedBox } from '@react-three/drei'
+import React from 'react'
 
-export const Box = () => {
-  return (
-    <>
-      {/** @ts-ignore */}
-      <RoundedBox scale={1} />
-    </>
-  )
-}
+export const Box = React.memo(
+  () => {
+    return (
+      <>
+        {/** @ts-ignore */}
+        <RoundedBox scale={1} />
+      </>
+    )
+  },
+  () => true,
+)
