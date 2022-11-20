abf9801f60c8b5b8ac3d9d57003768ac21a92b34
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-20 12:31:16 +0700

    feat: add models

--- src/layouts/ModelingLayout/Scene/Scene.tsx
+++ src/layouts/ModelingLayout/Scene/Scene.tsx
@@ -1,12 +1,11 @@
-import { Stars } from '@react-three/drei'
+import { PresentationControls, Stars } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
+import { Box } from '../Models'
 
 export const Scene = () => {
   return (
     <Canvas>
-      <ambientLight />
-
       <Stars
         radius={100}
         depth={50}
@@ -16,10 +15,16 @@ export const Scene = () => {
         fade
         speed={1}
       />
-      <mesh>
-        <boxGeometry />
-        <meshBasicMaterial color='red' />
-      </mesh>
+
+      <PresentationControls
+        snap
+        speed={2}
+        polar={[-Infinity, Infinity]}
+        azimuth={[-Infinity, Infinity]}
+        config={{ mass: 1, tension: 170, friction: 26 }}
+      >
+        <Box />
+      </PresentationControls>
     </Canvas>
   )
 }
