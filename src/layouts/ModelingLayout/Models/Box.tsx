cc4696aac45f89950a8e753a584fae3f0ac904c7
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-20 13:09:43 +0700

    feat: change models

--- src/layouts/ModelingLayout/Models/Box.tsx
+++ src/layouts/ModelingLayout/Models/Box.tsx
@@ -1,5 +1,17 @@
-import { RoundedBox } from '@react-three/drei'
+import { PresentationControls, RoundedBox } from '@react-three/drei'
 
 export const Box = () => {
-  return <RoundedBox />
+  return (
+    <mesh position={[-20, 0, 0]}>
+      <PresentationControls
+        snap
+        speed={2}
+        polar={[-Infinity, Infinity]}
+        azimuth={[-Infinity, Infinity]}
+        config={{ mass: 1, tension: 170, friction: 26 }}
+      >
+        <RoundedBox scale={1} />
+      </PresentationControls>
+    </mesh>
+  )
 }
--- src/layouts/ModelingLayout/Models/Planet.tsx
+++ src/layouts/ModelingLayout/Models/Planet.tsx
@@ -1,5 +1,17 @@
-import { Sphere } from '@react-three/drei'
+import { PresentationControls, Sphere } from '@react-three/drei'
 
 export const Planet = () => {
-  return <Sphere scale={2} />
+  return (
+    <PresentationControls
+      snap
+      speed={2}
+      polar={[-Infinity, Infinity]}
+      azimuth={[-Infinity, Infinity]}
+      config={{ mass: 1, tension: 170, friction: 26 }}
+    >
+      <Sphere scale={12}>
+        <meshBasicMaterial color='blue' />
+      </Sphere>
+    </PresentationControls>
+  )
 }
