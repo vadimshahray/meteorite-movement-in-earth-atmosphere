3173e3d4c0e148ecd3e7a539d7935c82f29b3264
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-21 10:19:50 +0700

    feat: fill data exiting planets

--- src/models/planets/mars.ts
+++ src/models/planets/mars.ts
@@ -3,13 +3,13 @@ import { G } from './constants'
 /** Дефолтная конфигурация планеты "Марс" */
 export const MarsPlanet: PlanetData = {
   M: {
-    value: 11,
-    power: 1,
-  }, // TODO: fill
+    value: 6.4171,
+    power: 23,
+  },
   R: {
-    value: 22,
-    power: 1,
-  }, // TODO: fill
+    value: 3389.5,
+    power: 3,
+  },
   g: {
     active: 'short',
     short: {
