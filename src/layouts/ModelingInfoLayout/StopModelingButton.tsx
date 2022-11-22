cd13e91d26c00383e6717560b71edc6c9e9f4644
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-22 19:06:10 +0700

    fix: don't show snackbar on stop modeling

--- src/layouts/ModelingInfoLayout/StopModelingButton.tsx
+++ src/layouts/ModelingInfoLayout/StopModelingButton.tsx
@@ -1,22 +1,13 @@
 import StopIcon from '@mui/icons-material/Stop'
 import { Button } from 'components'
-import { useSnackbar } from 'notistack'
 import { useDispatch } from 'react-redux'
 import { setIsModeling } from 'slices'
-import { infoSnackbar } from 'utils'
 
 export const StopModelingButton = () => {
   const dispatch = useDispatch()
 
-  const { enqueueSnackbar } = useSnackbar()
-
   const handleClick = () => {
     dispatch(setIsModeling(false))
-
-    enqueueSnackbar(
-      'Введенные данные содержат ошибки. Исправьте их!',
-      infoSnackbar('Невозможно начать моделирование!'),
-    )
   }
 
   return (
