7e32a922ab983aec0956a57aeed1e7b2467c1ccf
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2022-11-24 20:42:34 +0700

    feat: start timer when is modeling

--- src/layouts/InfoLayout/ModelingControl/ModelingTimer.tsx
+++ src/layouts/InfoLayout/ModelingControl/ModelingTimer.tsx
@@ -1,13 +1,20 @@
 import TimerOutlinedIcon from '@mui/icons-material/TimerOutlined'
 import { Box, Typography } from '@mui/material'
 import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { selectIsModeling } from 'selectors'
 
 const INTERVAL_STEP = 33
 
 export const ModelingTimer = () => {
+  const isModeling = useSelector(selectIsModeling)
   const [time, setTime] = useState('')
 
   useEffect(() => {
+    if (!isModeling) {
+      return
+    }
+
     const startTime = Date.now()
 
     const interval = setInterval(() => {
@@ -17,16 +24,16 @@ export const ModelingTimer = () => {
     }, INTERVAL_STEP)
 
     return () => clearInterval(interval)
-  }, [])
+  }, [isModeling])
 
-  return (
+  return isModeling ? (
     //@ts-ignore
     <Box display='flex' alignItems='center'>
       <TimerOutlinedIcon sx={{ marginRight: '2px' }} fontSize='small' />
 
       <Typography>{time}</Typography>
     </Box>
-  )
+  ) : null
 }
 
 const ticksToTime = (ms: number) => {
