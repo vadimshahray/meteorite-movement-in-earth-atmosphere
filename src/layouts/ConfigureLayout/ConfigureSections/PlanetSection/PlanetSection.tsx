c728f808dd73bb1689b4c7698a391ce7f05ede43
vadimshahray <vadimshahray.workmail@gmail.com>
2022-11-27 17:37:52 +0700

    feat: remove planet section toolbar buttons

--- src/layouts/ConfigureLayout/ConfigureSections/PlanetSection/PlanetSection.tsx
+++ src/layouts/ConfigureLayout/ConfigureSections/PlanetSection/PlanetSection.tsx
@@ -1,21 +1,9 @@
-import { useToolbar } from 'hooks'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { PlanetRField, PlanetMField } from './Fields'
-import { PickPlanetModelDataButton } from './PickPlanetModelDataButton'
 import { GVariableView } from './VariableViews'
 
 /** Секция конфигурации "Планета" */
 export const PlanetSection = () => {
-  const { setActionButtons } = useToolbar()
-
-  useEffect(() => {
-    setActionButtons([<PickPlanetModelDataButton />])
-
-    return () => {
-      setActionButtons()
-    }
-  }, [setActionButtons])
-
   return (
     <div>
       <PlanetRField />
