92b00b1de32aa1ffae86b6f77cc62b2190a7fc08
vadimshahray <vadimshahray.workmail@gmail.com>
2022-11-27 18:33:37 +0700

    feat: add `InfoSection` tab panel

---
+++ src/layouts/ConfigureLayout/ConfigureSections/InfoSection/InfoSecion.tsx
@@ -0,0 +1,11 @@
+import { Container } from 'components'
+import React from 'react'
+
+export const InfoSection = () => {
+  return (
+    <Container>
+      Данные Земли <br />
+      <br /> Данные атмосферы
+    </Container>
+  )
+}
