681a8263949cd979e5ba54417d3cdba9cf334c7c
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-07 00:57:25 +0700

    feat: add scale meters function

--- src/utils/scale.ts
+++ src/utils/scale.ts
@@ -3,3 +3,9 @@ const RADIUS_NORM = 1.0
 export const getRadiusNorm = (r: number) => {
   return RADIUS_NORM / r
 }
+
+const METER_SCALE = 0.00001
+
+export const scaleMeters = (n: number) => {
+  return n * METER_SCALE
+}
