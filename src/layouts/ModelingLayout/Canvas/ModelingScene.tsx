09e8657b99d9b298fdfcfc3149bcfeed769dcbd3
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-10 22:21:05 +0700

    feat: change camera position

--- src/layouts/ModelingLayout/Canvas/ModelingScene.tsx
+++ src/layouts/ModelingLayout/Canvas/ModelingScene.tsx
@@ -1,19 +1,19 @@
 import { useThree } from '@react-three/fiber'
 import { useSelector } from 'react-redux'
 import { selectIsModeling } from 'selectors'
-import { Earth, EARTH_RADIUS, Meteorite } from '../Models'
+import { Earth, Meteorite } from '../Models'
 
 export const ModelingScene = () => {
   const isModeling = useSelector(selectIsModeling)
 
   useThree(({ camera }) => {
-    camera.position.set(-2 * EARTH_RADIUS, 0, 0)
-    camera.rotation.set(0, -Math.PI / 2, 0)
-    // if (isModeling) {
-    // } else {
-    //   camera.rotation.set(0, 0, 0)
-    //   camera.position.set(0, 0, 5)
-    // }
+    if (isModeling) {
+      camera.position.set(-20, 0, 0)
+      camera.rotation.set(0, -Math.PI / 2, 0)
+    } else {
+      camera.rotation.set(0, 0, 0)
+      camera.position.set(-2, 0, 5)
+    }
   })
 
   return (
