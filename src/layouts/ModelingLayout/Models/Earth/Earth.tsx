0343d46df02a99adb8542fb7b6ae344e8de125f0
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-12 00:43:14 +0700

    feat: toggle Earth models depended theme

--- src/layouts/ModelingLayout/Models/Earth/Earth.tsx
+++ src/layouts/ModelingLayout/Models/Earth/Earth.tsx
@@ -2,9 +2,10 @@ import { Sphere, useGLTF } from '@react-three/drei'
 import { EARTH } from 'consts'
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { selectObjectModely0 } from 'selectors'
+import { selectObjectModely0, selectThemeColorMode } from 'selectors'
 import { getRadiusNorm, scaleMeters } from 'utils'
 import { EarthLight } from './EarthLight'
+import { EarthNight } from './EarthNight'
 
 const EARTH_MODEL_RADIUS = 1.7320494310588754
 
@@ -12,11 +13,14 @@ export const EARTH_RADIUS =
   getRadiusNorm(EARTH_MODEL_RADIUS) * scaleMeters(EARTH.RADIUS)
 
 export const Earth = React.memo(() => {
+  const colorMode = useSelector(selectThemeColorMode)
+
   const distance = scaleMeters(useSelector(selectObjectModely0))
 
   return (
     <group position={[EARTH_RADIUS + distance, 0, 0]}>
-      <EarthLight scale={EARTH_RADIUS} />
+      <EarthLight scale={EARTH_RADIUS} visible={colorMode === 'light'} />
+      <EarthNight scale={EARTH_RADIUS} visible={colorMode === 'dark'} />
 
       <Sphere scale={EARTH_RADIUS + 1}>
         <meshStandardMaterial color='#318ce7' opacity={0.19} transparent />
