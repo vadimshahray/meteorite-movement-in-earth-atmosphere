8f5418ac1dafcd1bcbfe2d0b5446cb7bbb7c2b64
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-11 20:29:42 +0700

    feat: add `Item` component

--- src/layouts/ModelingInfoLayout/CameraSettings/CameraSettings.tsx
+++ src/layouts/ModelingInfoLayout/CameraSettings/CameraSettings.tsx
@@ -1,16 +1,13 @@
 import VideocamIcon from '@mui/icons-material/Videocam'
-import { Box, Typography } from '@mui/material'
-import { Section } from 'components'
+import { Item, Section } from 'components'
 import { CameraToggler } from './CameraToggler'
 
 export const CameraSettings = () => {
   return (
     <Section title='Камера' Icon={VideocamIcon}>
-      {/*@ts-ignore*/}
-      <Box display='flex' justifyContent='space-between' alignItems='center'>
-        <Typography variant='subtitle2'>Положение камеры</Typography>
+      <Item label='Положение камеры'>
         <CameraToggler />
-      </Box>
+      </Item>
     </Section>
   )
 }
