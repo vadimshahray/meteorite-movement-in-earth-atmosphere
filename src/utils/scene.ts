dcea909cfe000346a86c112cb834ce8ff87ef1cb
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-11 14:22:56 +0700

    feat: add scene cameras info models

---
+++ src/utils/scene.ts
@@ -0,0 +1,14 @@
+export const sceneCameras: SceneCameraInfo[] = [
+  {
+    key: '@BackViewCamera',
+    name: 'Вид сзади',
+  },
+  {
+    key: '@SideViewCamera',
+    name: 'Вид сбоку',
+  },
+  {
+    key: '@EarthViewCamera',
+    name: 'Вид с Земли',
+  },
+]
