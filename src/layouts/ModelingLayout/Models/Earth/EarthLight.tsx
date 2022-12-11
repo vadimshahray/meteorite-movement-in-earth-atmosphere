75b3ff3eeebd1e838b84df6ec0e7a97c71263ce0
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-12 00:33:00 +0700

    chore: move Earth light model to itself directory

---
+++ src/layouts/ModelingLayout/Models/Earth/EarthLight.tsx
@@ -0,0 +1,29 @@
+import { useGLTF } from '@react-three/drei'
+import { MeshProps } from '@react-three/fiber'
+import * as THREE from 'three'
+import { GLTF } from 'three-stdlib'
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Sphere_Material002_0: THREE.Mesh
+  }
+  materials: {
+    ['Material.002']: THREE.MeshStandardMaterial
+  }
+}
+
+export const EarthLight = (props: MeshProps) => {
+  const { nodes, materials } = useGLTF(
+    '/models/earth/scene.gltf',
+  ) as unknown as GLTFResult
+
+  return (
+    <mesh
+      geometry={nodes.Sphere_Material002_0.geometry}
+      material={materials['Material.002']}
+      dispose={null}
+      {...props}
+      rotation={[-Math.PI / 3, -Math.PI / 6, Math.PI]}
+    />
+  )
+}
