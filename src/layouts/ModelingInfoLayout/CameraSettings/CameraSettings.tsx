d0e913f87281f0a131a00c9b527ac586810258f7
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-11 14:32:22 +0700

    style: customize camera settings

--- src/layouts/ModelingInfoLayout/CameraSettings/CameraSettings.tsx
+++ src/layouts/ModelingInfoLayout/CameraSettings/CameraSettings.tsx
@@ -1,3 +1,4 @@
+import VideocamIcon from '@mui/icons-material/Videocam'
 import { Box, Typography } from '@mui/material'
 import { Container } from 'components'
 import { CameraToggler } from './CameraToggler'
@@ -5,7 +6,10 @@ import { CameraToggler } from './CameraToggler'
 export const CameraSettings = () => {
   return (
     <Container disableGuttersH>
-      <Typography variant='subtitle1'>Камера</Typography>
+      <Typography variant='subtitle1' display='flex' alignItems='center'>
+        <VideocamIcon sx={{ marginRight: 1 }} />
+        Камера
+      </Typography>
 
       {/*@ts-ignore*/}
       <Box display='flex' justifyContent='space-between' alignItems='center'>
