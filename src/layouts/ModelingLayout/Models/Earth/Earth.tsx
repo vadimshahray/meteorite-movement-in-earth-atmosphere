1ab92a9bf09cbd82d27dc78421ba6c3faed0b207
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-12 11:19:50 +0700

    feat: add Earth position hook

--- src/layouts/ModelingLayout/Models/Earth/Earth.tsx
+++ src/layouts/ModelingLayout/Models/Earth/Earth.tsx
@@ -1,24 +1,22 @@
 import { Sphere, useGLTF } from '@react-three/drei'
 import { EARTH } from 'consts'
+import { useEarthPosition } from 'hooks'
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { selectObjectModely0, selectThemeColorMode } from 'selectors'
-import { getRadiusNorm, scaleMeters } from 'utils'
+import { selectThemeColorMode } from 'selectors'
 import { EarthLight } from './EarthLight'
 import { EarthNight } from './EarthNight'
 
 const EARTH_MODEL_RADIUS = 1.7320494310588754
 
-export const EARTH_RADIUS =
-  getRadiusNorm(EARTH_MODEL_RADIUS) * scaleMeters(EARTH.RADIUS)
+export const EARTH_RADIUS = (1.0 / EARTH_MODEL_RADIUS) * EARTH.RADIUS * 0.00001
 
 export const Earth = React.memo(() => {
   const colorMode = useSelector(selectThemeColorMode)
-
-  const distance = scaleMeters(useSelector(selectObjectModely0))
+  const earthPosition = useEarthPosition()
 
   return (
-    <group position={[EARTH_RADIUS + distance, 0, 0]}>
+    <group position={earthPosition}>
       <EarthLight scale={EARTH_RADIUS} visible={colorMode === 'light'} />
       <EarthNight scale={EARTH_RADIUS} visible={colorMode === 'dark'} />
 
