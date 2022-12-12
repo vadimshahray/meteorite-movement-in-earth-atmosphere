e409938ba2e91edeb6e53565b8cf58a21d312202
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-12 23:16:34 +0700

    refactor: ignore meteorite R in itself position

--- src/hooks/useMeteoritePosition.ts
+++ src/hooks/useMeteoritePosition.ts
@@ -3,11 +3,9 @@ import { useSelector } from 'react-redux'
 import { selectObjectModely0 } from 'selectors'
 import * as THREE from 'three'
 import { scaleMeters } from 'utils'
-import { useMeteoriteRadius } from './useMeteoirteRadius'
 
 export const useMeteoritePosition = () => {
   const y0 = scaleMeters(useSelector(selectObjectModely0))
-  const meteoriteRadius = useMeteoriteRadius().radius
 
-  return new THREE.Vector3(-EARTH_RADIUS - meteoriteRadius - y0, 0, 0)
+  return new THREE.Vector3(-EARTH_RADIUS - y0, 0, 0)
 }
