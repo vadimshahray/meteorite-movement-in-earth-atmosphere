16db07d1b2b228c6fe87fef5b3bf5ddb15a403e1
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-18 17:29:02 +0700

    feat: add meteorite modeling properties to slice

--- src/slices/modeling.slice.ts
+++ src/slices/modeling.slice.ts
@@ -1,7 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { setDefinedProperties } from 'utils'
 
 const initialState: ModelingSliceState = {
   isModeling: false,
+  meteorite: {
+    distance: 0,
+    velocity: 0,
+    xOffset: 0,
+  },
 }
 
 export const modelingSlice = createSlice<ModelingSliceState, ModelingSlice>({
@@ -11,7 +17,11 @@ export const modelingSlice = createSlice<ModelingSliceState, ModelingSlice>({
     setIsModeling: (state, { payload }) => {
       state.isModeling = payload
     },
+
+    setMeteoriteData: (state, { payload }) => {
+      setDefinedProperties(state.meteorite, payload)
+    },
   },
 })
 
-export const { setIsModeling } = modelingSlice.actions
+export const { setIsModeling, setMeteoriteData } = modelingSlice.actions
