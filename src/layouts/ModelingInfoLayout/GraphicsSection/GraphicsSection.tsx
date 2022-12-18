9199875539fb7db2989dca7cb0c7cc8c35a1fced
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-18 13:13:51 +0700

    feat: add graphics section

---
+++ src/layouts/ModelingInfoLayout/GraphicsSection/GraphicsSection.tsx
@@ -0,0 +1,6 @@
+import StackedLineChartIcon from '@mui/icons-material/StackedLineChart'
+import { Section } from 'components'
+
+export const GraphicsSection = () => {
+  return <Section title='Графики' Icon={StackedLineChartIcon}></Section>
+}
