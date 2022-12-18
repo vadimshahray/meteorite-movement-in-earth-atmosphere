039ce51e5fa877126b95440546d7bc0e0370a9ec
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-18 11:30:07 +0700

    chore: delete useless types

--- src/types/meteorite/meteorite.d.ts
+++ src/types/meteorite/meteorite.d.ts
@@ -1,24 +1,9 @@
 /** Модель физического объекта */
 type MeteoriteModel = {
-  /** Расстояние от поверхности планеты до центра масс объекта */
+  /** Расстояние от поверхности планеты до центра масс метеорита */
   distance: number
   /** Масса */
   mass: number
-  /** Радиус (не общее) */
+  /** Радиус */
   radius: number
 }
-
-/** Физические объекты, используемые в приложении */
-type PhysicalModels = '@Meteorite' | '@Ball' | '@Box'
-
-/** Физический объект. Используется для определения методов вычисления данных физ. объекта */
-type PhysicalObject = {} & MeteoriteModel
-
-/** Карта используемых свойств физической модели */
-type PhysicalModelPropertiesMap = Record<keyof MeteoriteModel, boolean>
-
-/** Карты используемых свойств физических моделей */
-type PhysicalModelsPropertiesMaps = Record<
-  PhysicalModels,
-  PhysicalModelPropertiesMap
->
