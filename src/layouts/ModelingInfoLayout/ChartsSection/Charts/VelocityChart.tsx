a3b366aa44169680e384a12067156c5ec40f3921
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-25 00:50:50 +0700

    feat: use formatter in charts

--- src/layouts/ModelingInfoLayout/ChartsSection/Charts/VelocityChart.tsx
+++ src/layouts/ModelingInfoLayout/ChartsSection/Charts/VelocityChart.tsx
@@ -1,9 +1,23 @@
 import { LinearChart } from 'components'
 import { useSelector } from 'react-redux'
 import { selectActiveChartPoints } from 'selectors'
+import { metersToVelocityString, ticksToString } from 'utils'
 
 export const VelocityChart = () => {
   const points = useSelector(selectActiveChartPoints)
 
-  return <LinearChart points={points} label='V(t)' xName='t' yName='V(t)' />
+  return (
+    <LinearChart
+      points={points}
+      label='V(t)'
+      xName='Время'
+      yName='Скорость'
+      xFormatter={(value) => {
+        return ticksToString(value as number)
+      }}
+      yFormatter={(value) => {
+        return metersToVelocityString(value as number)
+      }}
+    />
+  )
 }
