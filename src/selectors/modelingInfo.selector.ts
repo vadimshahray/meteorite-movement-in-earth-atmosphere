6d539950ef29b7ad80b13e40f7e9cf1973ebc8b9
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-24 20:11:56 +0700

    refactor: change modeling info slice

--- src/selectors/modelingInfo.selector.ts
+++ src/selectors/modelingInfo.selector.ts
@@ -1,2 +1,8 @@
 export const selectActiveChart = (state: RootState) =>
   state.modelingInfo.activeChart
+
+export const selectActiveChartPoints = (state: RootState) =>
+  state.modelingInfo.chartsPoints[selectActiveChart(state)]
+
+export const selectChartPoints = (chart: Charts) => (state: RootState) =>
+  state.modelingInfo.chartsPoints[chart]
