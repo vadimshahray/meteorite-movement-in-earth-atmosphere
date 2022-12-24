1c977f662b39cc18943368f1eea1ccb9723c32aa
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-24 22:11:07 +0700

    feat: use custom chart tooltip

--- src/layouts/ModelingInfoLayout/ChartsSection/Charts/DistanceChart.tsx
+++ src/layouts/ModelingInfoLayout/ChartsSection/Charts/DistanceChart.tsx
@@ -5,5 +5,5 @@ import { selectActiveChartPoints } from 'selectors'
 export const DistanceChart = () => {
   const points = useSelector(selectActiveChartPoints)
 
-  return <LinearChart points={points} label='D(t)' />
+  return <LinearChart points={points} label='D(t)' xName='t' yName='D(t)' />
 }
--- src/layouts/ModelingInfoLayout/ChartsSection/Charts/VelocityChart.tsx
+++ src/layouts/ModelingInfoLayout/ChartsSection/Charts/VelocityChart.tsx
@@ -5,5 +5,5 @@ import { selectActiveChartPoints } from 'selectors'
 export const VelocityChart = () => {
   const points = useSelector(selectActiveChartPoints)
 
-  return <LinearChart points={points} label='V(t)' />
+  return <LinearChart points={points} label='V(t)' xName='t' yName='V(t)' />
 }
