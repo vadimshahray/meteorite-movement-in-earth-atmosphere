8323271c3c32e30cfa04b9e5cde3e66c1d1805a9
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-24 12:52:16 +0700

    refactor: show modeling timer if isModeling

--- src/layouts/InfoLayout/ModelingControl/ModelingControl.tsx
+++ src/layouts/InfoLayout/ModelingControl/ModelingControl.tsx
@@ -1,12 +1,17 @@
 import { Container } from 'components'
+import { useSelector } from 'react-redux'
+import { selectIsModeling } from 'selectors'
 import { ModelingButton } from './ModelingButton'
 import { ModelingTimer } from './ModelingTimer'
 
 export const ModelingControl = () => {
+  const isModeling = useSelector(selectIsModeling)
+
   return (
     <Container sx={{ display: 'flex', justifyContent: 'space-between' }}>
       <ModelingButton />
-      <ModelingTimer />
+
+      {isModeling && <ModelingTimer />}
     </Container>
   )
 }
