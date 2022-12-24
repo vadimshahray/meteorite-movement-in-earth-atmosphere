b0fa4781144270158ec4483781a75d84848edc38
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-24 12:52:42 +0700

    refactor: use timer slice in `ModelingTimer`

--- src/layouts/InfoLayout/ModelingControl/ModelingTimer.tsx
+++ src/layouts/InfoLayout/ModelingControl/ModelingTimer.tsx
@@ -1,48 +1,27 @@
 import TimerOutlinedIcon from '@mui/icons-material/TimerOutlined'
 import { Box, Typography } from '@mui/material'
-import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { selectIsModeling } from 'selectors'
-
-const INTERVAL_STEP = 33
+import { selectModelingTimer } from 'selectors'
 
 export const ModelingTimer = () => {
-  const isModeling = useSelector(selectIsModeling)
-  const [time, setTime] = useState('')
+  const { hours, minutes, seconds, milliseconds } =
+    useSelector(selectModelingTimer)
 
-  useEffect(() => {
-    if (!isModeling) return
-
-    const startTime = Date.now()
-
-    const interval = setInterval(() => {
-      const elapsedTime = Date.now() - startTime
-
-      setTime(ticksToTime(elapsedTime))
-    }, INTERVAL_STEP)
-
-    return () => clearInterval(interval)
-  }, [isModeling])
-
-  return isModeling ? (
-    //@ts-ignore
-    <Box display='flex' alignItems='center'>
-      <TimerOutlinedIcon sx={{ marginRight: '2px' }} fontSize='small' />
-
-      <Typography fontFamily='Fira Code'>{time}</Typography>
-    </Box>
-  ) : null
-}
-
-const ticksToTime = (ms: number) => {
-  const hours = Math.floor(ms / 1000 / 60 / 60)
-  const minutes = Math.floor(ms / 1000 / 60) % 60
-  const seconds = Math.floor(ms / 1000) % 60
-
-  return `${addZeros(hours, 2)}:${addZeros(minutes, 2)}:${addZeros(
+  const timeString = `${addZeros(hours, 2)}:${addZeros(minutes, 2)}:${addZeros(
     seconds,
     2,
-  )}:${addZeros(ms % 1000, 3)}`
+  )}:${addZeros(milliseconds, 3)}`
+
+  return (
+    <>
+      {/**@ts-ignore */}
+      <Box display='flex' alignItems='center'>
+        <TimerOutlinedIcon sx={{ marginRight: '2px' }} fontSize='small' />
+
+        <Typography fontFamily='Fira Code'>{timeString}</Typography>
+      </Box>
+    </>
+  )
 }
 
 const addZeros = (x: number, count: number) => {
