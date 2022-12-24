64115324734133373a024e831cc878961b041721
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-24 12:51:48 +0700

    feat: add modeling timer to slice

--- src/slices/modeling.slice/modeling.async.slice.ts
+++ src/slices/modeling.slice/modeling.async.slice.ts
@@ -1,8 +1,47 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
+import { setTimerData } from './modeling.slice'
 
-export const startModeling = createAsyncThunk(
-  'modeling/startModeling',
-  () => {},
-)
+export const startModeling = createAsyncThunk<
+  void,
+  void,
+  { dispatch: AppDispatch }
+>('modeling/startModeling', (_, { dispatch }) => {
+  dispatch(startModelingTimer(() => {}))
+})
 
-export const stopModeling = createAsyncThunk('modeling/stopModeling', () => {})
+export const stopModeling = createAsyncThunk<
+  void,
+  void,
+  { dispatch: AppDispatch }
+>('modeling/stopModeling', (_, { dispatch }) => {
+  dispatch(stopModelingTimer())
+})
+
+const TIMER_INTERVAL = 33
+
+let interval: NodeJS.Timer
+
+const startModelingTimer = createAsyncThunk<
+  void,
+  () => void,
+  { dispatch: AppDispatch }
+>('modeling/startTimer', (callback, { dispatch }) => {
+  let ticks = 0
+
+  interval = setInterval(() => {
+    callback()
+
+    ticks += TIMER_INTERVAL
+
+    const hours = Math.floor(ticks / 1000 / 60 / 60)
+    const minutes = Math.floor(ticks / 1000 / 60) % 60
+    const seconds = Math.floor(ticks / 1000) % 60
+    const milliseconds = ticks % 1000
+
+    dispatch(setTimerData({ hours, minutes, seconds, milliseconds, ticks }))
+  }, TIMER_INTERVAL)
+})
+
+const stopModelingTimer = createAsyncThunk('modeling/stopTimer', () => {
+  clearInterval(interval)
+})
