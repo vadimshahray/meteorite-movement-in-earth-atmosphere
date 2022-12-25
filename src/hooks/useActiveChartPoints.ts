3a9763922adde67bdadedc907591ddd5bb938aba
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-25 22:28:25 +0700

    feat: use charts total points

---
+++ src/hooks/useActiveChartPoints.ts
@@ -0,0 +1,15 @@
+import { useSelector } from 'react-redux'
+import {
+  selectActiveChartLastPoints,
+  selectActiveChartTotalPoints,
+  selectModelingStatus,
+} from 'selectors'
+
+export const useActiveChartPoints = () => {
+  const modelingStatus = useSelector(selectModelingStatus)
+
+  const lastPoints = useSelector(selectActiveChartLastPoints)
+  const totalPoints = useSelector(selectActiveChartTotalPoints)
+
+  return modelingStatus === 'processing' ? lastPoints : totalPoints
+}
