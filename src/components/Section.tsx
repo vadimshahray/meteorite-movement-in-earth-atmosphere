9ae0fd7d3f666ebdb3876c2dc148fcfa21cc1dbf
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-25 20:15:12 +0700

    feat: make `Section` collapsible

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,21 +1,42 @@
-import { Typography } from '@mui/material'
-import { PropsWithChildren } from 'react'
+import { ExpandLess, ExpandMore } from '@mui/icons-material'
+import { Collapse, IconButton, Stack, Typography } from '@mui/material'
+import { useState, PropsWithChildren } from 'react'
 import { Container } from './Container'
 
 export type SectionProps = {
   title: string
   Icon?: Icon
+  collapsible?: boolean
 } & PropsWithChildren
 
-export const Section = ({ title, Icon, children }: SectionProps) => {
+export const Section = ({
+  title,
+  Icon,
+  children,
+  collapsible,
+}: SectionProps) => {
+  const [expanded, setExpanded] = useState(true)
+
+  const handleClick = () => {
+    setExpanded(!expanded)
+  }
+
   return (
     <Container disableGuttersH>
-      <Typography variant='subtitle1' display='flex' alignItems='center'>
-        {Icon && <Icon sx={{ marginRight: 1 }} />}
-        {title}
-      </Typography>
+      <Stack direction='row' alignItems='center' justifyContent='space-between'>
+        <Typography variant='subtitle1' display='flex' alignItems='center'>
+          {Icon && <Icon sx={{ marginRight: 1 }} />}
+          {title}
+        </Typography>
 
-      {children}
+        {collapsible && (
+          <IconButton onClick={handleClick}>
+            {expanded ? <ExpandLess /> : <ExpandMore />}
+          </IconButton>
+        )}
+      </Stack>
+
+      <Collapse in={expanded}>{children}</Collapse>
     </Container>
   )
 }
