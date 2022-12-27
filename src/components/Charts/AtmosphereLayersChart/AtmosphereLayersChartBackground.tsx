2a79a3b4288cedf3f2fc715da0fa37da46609b1a
vadimshahray <vadimshahray.workmail@gmail.com>
2022-12-27 23:32:50 +0700

    refactor: use 1 area for atmosphere layers

--- src/components/Charts/AtmosphereLayersChart/AtmosphereLayersChartBackground.tsx
+++ src/components/Charts/AtmosphereLayersChart/AtmosphereLayersChartBackground.tsx
@@ -12,11 +12,7 @@ const transformCoefficient = 1000
 export const AtmosphereLayersChartBackground = ({
   maxY,
 }: AtmosphereLayersChartBackgroundProps) => {
-  const exosphere_gradient = 'exosphere_layer_gradient'
-  const thermosphere_gradient = 'thermosphere_layer_gradient'
-  const mesosphere_gradient = 'mesosphere_layer_gradient'
-  const stratosphere_gradient = 'stratosphere_layer_gradient'
-  const troposphere_gradient = 'troposphere_layer_gradient'
+  const atmosphereLayersGradient = 'atmosphere_layers_gradient'
 
   const { chartBaseProps } = useBaseChartElementsProps()
 
@@ -43,69 +39,19 @@ export const AtmosphereLayersChartBackground = ({
         <YAxis {...yAxisBaseProps} hide />
 
         <defs>
-          <linearGradient id={exosphere_gradient} {...linearGradientBaseProps}>
+          <linearGradient
+            id={atmosphereLayersGradient}
+            {...linearGradientBaseProps}
+          >
             <stop stopColor='#1450be' {...stopTopBaseProps} />
-            <stop stopColor='#1450be' {...stopBottomBaseProps} />
-          </linearGradient>
-
-          <linearGradient
-            id={thermosphere_gradient}
-            {...linearGradientBaseProps}
-          >
-            <stop stopColor='#4a87ff' {...stopTopBaseProps} />
-            <stop stopColor='#4a87ff' {...stopBottomBaseProps} />
-          </linearGradient>
-
-          <linearGradient id={mesosphere_gradient} {...linearGradientBaseProps}>
-            <stop stopColor='#00c1f2' {...stopTopBaseProps} />
-            <stop stopColor='#00c1f2' {...stopBottomBaseProps} />
-          </linearGradient>
-
-          <linearGradient
-            id={stratosphere_gradient}
-            {...linearGradientBaseProps}
-          >
-            <stop stopColor='#69d7f8' {...stopTopBaseProps} />
-            <stop stopColor='#69d7f8' {...stopBottomBaseProps} />
-          </linearGradient>
-
-          <linearGradient
-            id={troposphere_gradient}
-            {...linearGradientBaseProps}
-          >
-            <stop stopColor='#98e5f9' {...stopTopBaseProps} />
-            <stop stopColor='#98e5f9' {...stopTopBaseProps} />
+            <stop stopColor='#98e5f9' {...stopBottomBaseProps} />
           </linearGradient>
         </defs>
 
         <Area
           dataKey='exosphere'
-          fill={`url(#${exosphere_gradient})`}
+          fill={`url(#${atmosphereLayersGradient})`}
           stroke='#1450be'
-          {...areaCommonProps}
-        />
-        <Area
-          dataKey='thermosphere'
-          fill={`url(#${thermosphere_gradient})`}
-          stroke='#4a87ff'
-          {...areaCommonProps}
-        />
-        <Area
-          dataKey='mesosphere'
-          fill={`url(#${mesosphere_gradient})`}
-          stroke='#00c1f2'
-          {...areaCommonProps}
-        />
-        <Area
-          dataKey='stratosphere'
-          fill={`url(#${stratosphere_gradient})`}
-          stroke='#69d7f8'
-          {...areaCommonProps}
-        />
-        <Area
-          dataKey='troposphere'
-          fill={`url(#${troposphere_gradient})`}
-          stroke='#98e5f9'
           {...areaCommonProps}
         />
 
