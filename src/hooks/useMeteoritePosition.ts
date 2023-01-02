0989f63b7ccd81ac67515c6783f011d55a1eec23
vadimshahray <vadimshahray.workmail@gmail.com>
2023-01-02 22:38:27 +0700

    feat: modeling meteorite

--- src/hooks/useMeteoritePosition.ts
+++ src/hooks/useMeteoritePosition.ts
@@ -1,15 +1,25 @@
 import { useSelector } from 'react-redux'
-import { selectMeteoriteDistance } from 'selectors'
+import {
+  selectMeteoriteDistance,
+  selectModelingMeteoriteDistance,
+  selectModelingStatus,
+} from 'selectors'
 import * as THREE from 'three'
 import { scaleMeters } from 'utils'
 import { useEarthRadius } from './useEarthRadius'
 import { useMeteoriteRadius } from './useMeteoirteRadius'
 
 export const useMeteoritePosition = () => {
+  const modelingStatus = useSelector(selectModelingStatus)
+
   const EarthRadius = useEarthRadius()
   const meteoriteRadius = useMeteoriteRadius()
 
-  const distance = useSelector(selectMeteoriteDistance)
+  const distance = useSelector(
+    modelingStatus === 'idle'
+      ? selectMeteoriteDistance
+      : selectModelingMeteoriteDistance,
+  )
 
   return new THREE.Vector3(
     0,
