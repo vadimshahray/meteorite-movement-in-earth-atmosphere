c6cfb238e5252667829d4b3fe66fee1c457e4d47
vadimshahray <vadimshahray.workmail@gmail.com>
2023-01-02 13:08:49 +0700

    chore: rename const

--- src/hooks/useEarthRadius.ts
+++ src/hooks/useEarthRadius.ts
@@ -1,5 +1,5 @@
 import { EARTH_LIGHT_MODEL_RADIUS } from 'layouts/ModelingLayout/Models/Earth/EarthLight'
-import { EARTH_NIGHT_MODEL_RADUIS } from 'layouts/ModelingLayout/Models/Earth/EarthNight'
+import { EARTH_NIGHT_MODEL_RADIUS } from 'layouts/ModelingLayout/Models/Earth/EarthNight'
 import { useSelector } from 'react-redux'
 import { selectThemeColorMode } from 'selectors'
 
@@ -8,5 +8,5 @@ export const useEarthRadius = () => {
 
   return colorMode === 'light'
     ? EARTH_LIGHT_MODEL_RADIUS
-    : EARTH_NIGHT_MODEL_RADUIS
+    : EARTH_NIGHT_MODEL_RADIUS
 }
