9f64e0714251a6ac24f41fd626d7584b4e6e21e0
vadimshahray <vadimshahray.workmail@gmail.com>
2023-01-06 21:19:02 +0700

    feat: add velocity physic function

--- src/slices/modeling.slice/modelingCalculation.async.slice.ts
+++ src/slices/modeling.slice/modelingCalculation.async.slice.ts
@@ -3,6 +3,8 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import { CALCULATION_INTERVAL_MS } from '@constants'
 import { finishModeling, setModelingChartsPoints } from '@slices'
 import {
+  selectMeteoriteMass,
+  selectMeteoriteRadius,
   selectModelingMeteoriteDistance,
   selectModelingMeteoriteVelocity,
 } from '@selectors'
@@ -22,31 +24,112 @@ export const calculateMeteoriteData = createAsyncThunk<
   }
 
   await dispatch(calculateMeteoriteVelocity())
-  await dispatch(calculateMeteoriteDistance())
   await dispatch(calculateCollisionTime())
 })
+
+let O = -10
 
 export const calculateMeteoriteVelocity = createAsyncThunk<
   number,
   void,
   { state: RootState }
->('modeling/calculateMeteoriteVelocity', (_, { getState }) => {
-  const Vi = selectModelingMeteoriteVelocity(getState())
+>('modeling/calculateMeteoriteVelocity', async (_, { getState, dispatch }) => {
+  let m = selectMeteoriteMass(getState()),
+    v = selectModelingMeteoriteVelocity(getState()),
+    H = selectModelingMeteoriteDistance(getState()),
+    r = selectMeteoriteRadius(getState()),
+    h = CALCULATION_INTERVAL_MS,
+    k1 = 0,
+    k2 = 0,
+    k3 = 0,
+    k4 = 0,
+    m1 = 0,
+    m2 = 0,
+    m3 = 0,
+    m4 = 0,
+    l1 = 0,
+    l2 = 0,
+    l3 = 0,
+    l4 = 0,
+    Rp = 6371000,
+    g0 = 9.8
 
-  return Vi + 7
+  k1 =
+    (-h *
+      ((Math.PI * r * r) / m / ((g0 * Rp) / (Rp + H))) *
+      (1.22 * Math.pow(Math.E, -0.000129 * H)) *
+      v *
+      v) /
+      2 -
+    ((g0 * Rp) / (Rp + H)) * sin(O)
+  m1 = h * cos(O) * (v / (Rp + H) - (g0 * Rp) / (Rp + H) / v)
+  l1 = h * v * sin(O)
+
+  k2 =
+    (-h *
+      ((Math.PI * r * r) / m / ((g0 * Rp) / (Rp + H + 0.5 * l1))) *
+      (1.22 * Math.pow(Math.E, -0.000129 * (H + 0.5 * l1))) *
+      (v + 0.5 * k1) *
+      (v + 0.5 * k1)) /
+      2 -
+    ((g0 * Rp) / (Rp + H + 0.5 * l1)) * sin(O + 0.5 * m1)
+  m2 =
+    h *
+    cos(O + 0.5 * m1) *
+    ((v + 0.5 * k1) / (Rp + H + 0.5 * l1) -
+      (g0 * Rp) / (Rp + H + 0.5 * l1) / (v + 0.5 * k1))
+  l2 = h * (v + 0.5 * k1) * sin(O + 0.5 * m1)
+
+  k3 =
+    (-h *
+      ((Math.PI * r * r) / m / ((g0 * Rp) / (Rp + H + 0.5 * l2))) *
+      (1.22 * Math.pow(Math.E, -0.000129 * (H + 0.5 * l2))) *
+      (v + 0.5 * k2) *
+      (v + 0.5 * k2)) /
+      2 -
+    ((g0 * Rp) / (Rp + H + 0.5 * l2)) * sin(O + 0.5 * m2)
+  m3 =
+    h *
+    cos(O + 0.5 * m2) *
+    ((v + 0.5 * k2) / (Rp + H + 0.5 * l2) -
+      (g0 * Rp) / (Rp + H + 0.5 * l2) / (v + 0.5 * k2))
+  l3 = h * (v + 0.5 * k2) * sin(O + 0.5 * m2)
+
+  k4 =
+    (-h *
+      ((Math.PI * r * r) / m / ((g0 * Rp) / (Rp + H + l3))) *
+      (1.22 * Math.pow(Math.E, -0.000129 * (H + l3))) *
+      (v + k3) *
+      (v + k3)) /
+      2 -
+    ((g0 * Rp) / (Rp + H + l3)) * sin(O + m3)
+  m4 =
+    h *
+    cos(O + m3) *
+    ((v + k3) / (Rp + H + l3) - (g0 * Rp) / (Rp + H + l3) / (v + k3))
+  l4 = h * (v + k3) * sin(O + m3)
+
+  v = v + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+  O = O + (m1 + 2 * m2 + 2 * m3 + m4) / 6
+  H = H + (l1 + 2 * l2 + 2 * l3 + l4) / 6
+
+  await dispatch(calculateMeteoriteDistance(H))
+
+  return v
 })
+
+const sin = (n: number) => {
+  return Math.sin((n * Math.PI) / 180)
+}
+const cos = (n: number) => {
+  return Math.cos((n * Math.PI) / 180)
+}
 
 export const calculateMeteoriteDistance = createAsyncThunk<
   number,
-  void,
+  number,
   { state: RootState }
->('modeling/calculateMeteoriteDistance', (_, { getState }) => {
-  const velocity = selectModelingMeteoriteVelocity(getState())
-  const Dp = selectModelingMeteoriteDistance(getState())
-
-  // Переводим секунды в миллисекунду, потому что скорость измеряется в м/с
-  const D = Dp - (velocity * CALCULATION_INTERVAL_MS) / 1000
-
+>('modeling/calculateMeteoriteDistance', (D, { getState }) => {
   return D > 0 ? D : 0
 })
 
