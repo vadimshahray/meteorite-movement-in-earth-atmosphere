d75eccc6f1dabb440c6218fe98f2c1a6d8926c73
vadimshahray <vadimshahray.workmail@gmail.com>
2023-01-07 13:09:08 +0700

    chore: add prettier ignore to physics formula

--- src/slices/modeling.slice/modelingCalculation.async.slice.ts
+++ src/slices/modeling.slice/modelingCalculation.async.slice.ts
@@ -40,65 +40,30 @@ export const calculateMeteoriteMovement = createAsyncThunk<
   const H = selectModelingMeteoriteDistance(getState())
   const O = selectModelingMeteoriteLocalSkylineAngle(getState())
 
-  const k1 =
-    (-h *
-      ((Math.PI * r * r) / m / ((g0 * Rp) / (Rp + H))) *
-      (1.22 * Math.pow(Math.E, -0.000129 * H)) *
-      v *
-      v) /
-      2 -
-    ((g0 * Rp) / (Rp + H)) * sin(O)
+  // prettier-ignore
+  const k1 = (-h * ((Math.PI * r * r) / m / ((g0 * Rp) / (Rp + H))) * (1.22 * Math.pow(Math.E, -0.000129 * H)) * v * v) / 2 - ((g0 * Rp) / (Rp + H)) * sin(O)
 
   const m1 = h * cos(O) * (v / (Rp + H) - (g0 * Rp) / (Rp + H) / v)
 
   const l1 = h * v * sin(O)
+  // prettier-ignore
+  const k2 = (-h * ((Math.PI * r * r) / m / ((g0 * Rp) / (Rp + H + 0.5 * l1))) * (1.22 * Math.pow(Math.E, -0.000129 * (H + 0.5 * l1))) * (v + 0.5 * k1) * (v + 0.5 * k1)) / 2 - ((g0 * Rp) / (Rp + H + 0.5 * l1)) * sin(O + 0.5 * m1)
 
-  const k2 =
-    (-h *
-      ((Math.PI * r * r) / m / ((g0 * Rp) / (Rp + H + 0.5 * l1))) *
-      (1.22 * Math.pow(Math.E, -0.000129 * (H + 0.5 * l1))) *
-      (v + 0.5 * k1) *
-      (v + 0.5 * k1)) /
-      2 -
-    ((g0 * Rp) / (Rp + H + 0.5 * l1)) * sin(O + 0.5 * m1)
+  // prettier-ignore
+  const m2 = h * cos(O + 0.5 * m1) * ((v + 0.5 * k1) / (Rp + H + 0.5 * l1) - (g0 * Rp) / (Rp + H + 0.5 * l1) / (v + 0.5 * k1))
 
-  const m2 =
-    h *
-    cos(O + 0.5 * m1) *
-    ((v + 0.5 * k1) / (Rp + H + 0.5 * l1) -
-      (g0 * Rp) / (Rp + H + 0.5 * l1) / (v + 0.5 * k1))
+  // prettier-ignore
   const l2 = h * (v + 0.5 * k1) * sin(O + 0.5 * m1)
-
-  const k3 =
-    (-h *
-      ((Math.PI * r * r) / m / ((g0 * Rp) / (Rp + H + 0.5 * l2))) *
-      (1.22 * Math.pow(Math.E, -0.000129 * (H + 0.5 * l2))) *
-      (v + 0.5 * k2) *
-      (v + 0.5 * k2)) /
-      2 -
-    ((g0 * Rp) / (Rp + H + 0.5 * l2)) * sin(O + 0.5 * m2)
-
-  const m3 =
-    h *
-    cos(O + 0.5 * m2) *
-    ((v + 0.5 * k2) / (Rp + H + 0.5 * l2) -
-      (g0 * Rp) / (Rp + H + 0.5 * l2) / (v + 0.5 * k2))
+  // prettier-ignore
+  const k3 = (-h * ((Math.PI * r * r) / m / ((g0 * Rp) / (Rp + H + 0.5 * l2))) * (1.22 * Math.pow(Math.E, -0.000129 * (H + 0.5 * l2))) * (v + 0.5 * k2) * (v + 0.5 * k2)) / 2 - ((g0 * Rp) / (Rp + H + 0.5 * l2)) * sin(O + 0.5 * m2)
+  // prettier-ignore
+  const m3 = h * cos(O + 0.5 * m2) * ((v + 0.5 * k2) / (Rp + H + 0.5 * l2) - (g0 * Rp) / (Rp + H + 0.5 * l2) / (v + 0.5 * k2))
 
   const l3 = h * (v + 0.5 * k2) * sin(O + 0.5 * m2)
-
-  const k4 =
-    (-h *
-      ((Math.PI * r * r) / m / ((g0 * Rp) / (Rp + H + l3))) *
-      (1.22 * Math.pow(Math.E, -0.000129 * (H + l3))) *
-      (v + k3) *
-      (v + k3)) /
-      2 -
-    ((g0 * Rp) / (Rp + H + l3)) * sin(O + m3)
-
-  const m4 =
-    h *
-    cos(O + m3) *
-    ((v + k3) / (Rp + H + l3) - (g0 * Rp) / (Rp + H + l3) / (v + k3))
+  // prettier-ignore
+  const k4 = (-h * ((Math.PI * r * r) / m / ((g0 * Rp) / (Rp + H + l3))) * (1.22 * Math.pow(Math.E, -0.000129 * (H + l3))) * (v + k3) * (v + k3)) / 2 - ((g0 * Rp) / (Rp + H + l3)) * sin(O + m3)
+  // prettier-ignore
+  const m4 = h * cos(O + m3) * ((v + k3) / (Rp + H + l3) - (g0 * Rp) / (Rp + H + l3) / (v + k3))
 
   const l4 = h * (v + k3) * sin(O + m3)
 
